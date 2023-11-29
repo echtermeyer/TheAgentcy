a9b995251048ef5c282edb011c47d58a343e2760
lasse <lasse.friedrich.de@gmail.com>
2023-11-29 17:41:19 +0100

    kack auf logs

--- sandbox/instantiate.py
+++ sandbox/instantiate.py
@@ -1,15 +1,14 @@
 import logging
 import os
+import time
 from src.utils import write_str_to_file
 from sandbox.dockergenerator import execute_python
-from sandbox.logger import setup_logger
 import docker 
 
-class PythonSandbox:
+class Sandbox:
     """
-    A class for creating and managing a Python sandbox environment using Docker.
+    A class for creating and sandbox environments using Docker.
     """
-
     def __init__(self, subfolder_path: str = "backend") -> None:
         """
         Initializes the Python sandbox environment.
@@ -22,9 +21,20 @@ class PythonSandbox:
         if not os.path.exists(self.directory_path):
             os.makedirs(self.directory_path)
 
-        setup_logger(self.directory_path)
+    @property
+    def path(self):
+        return self.directory_path
+    
+    @property
+    def type(self):
+        return self.directory_path
+    
+    pass
 
-
+class PythonSandbox(Sandbox):
+    """
+    A class for creating and managing a Python sandbox environment using Docker.
+    """
     def trigger_execution_pipeline(self, fulltext_code: str) -> docker.models.containers.Container:
         """
         Triggers the execution pipeline for the given Python code.
@@ -38,6 +48,7 @@ class PythonSandbox:
         logging.info(f"New Python Pipeline request for code: {fulltext_code}")
         file_path = write_str_to_file(fulltext_code, self.directory_path, ".py")
         running_container = execute_python(file_path)
+        time.sleep(1) # breathing time so logs can be displayed
         return running_container
 
     def init_basic_environment(self) -> docker.models.containers.Container:
--- sandbox_test.py
+++ sandbox_test.py
@@ -1,6 +1,6 @@
-import logging
-import threading
 from sandbox.instantiate import PythonSandbox
+from sandbox.logger import Logger
+
 
 sandbox = PythonSandbox()
 
@@ -13,25 +13,9 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"Quick Cahange":"David"}
+    return "Welcome to my life!"
 
 # Start the web server
 uvicorn.run(app, host="0.0.0.0", port=8000)""")
 
-# Stream and print container logs
-def stream_docker_logs(container):
-    """
-    Streams logs from a Docker container.
-
-    Args:
-        container (docker.models.containers.Container): The Docker container to stream logs from.
-    """
-    try:
-        for log_line in container.logs(stream=True):
-            logging.info(f"Container {container.short_id}: {log_line.decode('utf-8', 'ignore').strip()}")
-    except Exception as e:
-        logging.error(f"Error streaming logs from container {container.short_id}: {str(e)}")
-
-# Start a new thread to stream the logs
-log_thread = threading.Thread(target=stream_docker_logs, args=(container,))
-log_thread.start()
+print(container.logs(tail=10).decode('utf-8'))
