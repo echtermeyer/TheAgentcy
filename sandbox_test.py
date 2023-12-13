f88969a77652c927d3484fbb0a8bc7fb26d78e31
lasse <lasse.friedrich.de@gmail.com>
2023-12-13 15:42:32 +0100

    CORS support

--- sandbox_test.py
+++ sandbox_test.py
@@ -9,13 +9,23 @@ startup_logger = Logger()
 
 backend_string = """
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 app = FastAPI()
 
+# Add CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
 @app.get("/test")
 async def read_root():
-    return "Backend running!"
+    return {"message": "Backend running!"}
 
 # Start the web server
 uvicorn.run(app, host="0.0.0.0", port=8000)
@@ -51,7 +61,7 @@ frontend_string = """
     <script>
         // Embedded JavaScript
         document.getElementById('myButton').addEventListener('click', function() {
-            fetch('http://172.20.0.2:8000/test')
+            fetch('http://localhost:8000/test')
                 .then(response => response.text())
                 .then(data => {
                     document.getElementById('apiResponse').textContent = data;
