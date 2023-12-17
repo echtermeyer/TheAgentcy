7055074d176efdac10f4731fa6394b3d13a3a167
benediktpri <felix.2001@web.de>
2023-12-17 14:32:18 +0100

    updated prompts and new parsers for code

--- src/pipeline.py
+++ src/pipeline.py
@@ -46,39 +46,52 @@ class Pipeline:
 
         # Start database development
         conv2 = ConversationWrapper(self.database_dev, self.database_test)
-        code_database = conv2.start(tasks["database"])  # do something with the code
+        code_database_str = conv2.start("These are the requirements: "+tasks["database"]+' '+output_format('sql', True))  
+        code_database = extract_code(code_database_str, 'sql') # do something with the code
 
         # Documentation for database
         conv3 = ConversationWrapper(self.database_doc, self.database_test)
         starter = f"""
-        Write down questions you need to know in order to create an excellent and detailed documentation for the
+        Write down questions you need to know in order to create an detailed documentation for the
         database layer of the application. The project you are working on is about: {tasks['database']}.
         """
         documentation_database = conv3.start(starter)
-        self.orchestrator.inject_message(documentation_database, kind="human")
+        self.orchestrator.inject_message(documentation_database, kind="human") #QUESTION: what is this doing ??
 
         # Start backend development
         conv4 = ConversationWrapper(self.backend_dev, self.backend_test)
-        code_database = conv4.start(tasks["backend"])  # do something with the code
+        code_backend_str = conv4.start("These are the requirements: "+tasks["backend"]+' '+output_format('python', True))  
+        code_backend = extract_code(code_backend_str, 'python') # do something with the code
+
+        #TODO: what about the error logs? they still need to be added to conversation at some point. 
+        # In my opinon conversation starts too early. Backend Dev should first develop, then code is executed by us, then conversation with tester starts. 
+        # Opposed to initial design the tester now has two "roles": testing and answering to the doc. Maybe define seperate role prompts?
 
         # Documentation for backend
         conv5 = ConversationWrapper(self.backend_doc, self.backend_test)
         starter = f"""
         Write down questions you need to know in order to create an excellent and detailed documentation for the 
         backend layer of the application. The project you are working on is about: {tasks['backend']}.
-        """
+        """#TODO: maybe provide actual project description instead of task
         documentation_backend = conv5.start(starter)
-        self.orchestrator.inject_message(documentation_backend, kind="human")
+        self.orchestrator.inject_message(documentation_backend, kind="human") #QUESTION: what is this doing ??
+        #QUESTION: I am unsure when the code that is supposed to be documented enters this conversation? Is this missing?
+        # Or is the idea that the tester alone can look at the code from prev. conversation and answers are generated this way?
+        # Wouldn't we get much better answers if we just give the code to the doc aswell and do not do a conversation here?
 
         # Start backend development
         conv6 = ConversationWrapper(self.frontend_dev, self.frontend_test)
-        code_database = conv6.start(tasks["database"])  # do something with the code
+        code_frontend_str = conv6.start("These are the requirements: "+tasks["database"]+output_format('html',True)+"Do this aswell for the 'css' and 'javascript' code.")  # do something with the code
+        code_frontend_html = extract_code(code_frontend_str, 'html')
+        code_frontend_css = extract_code(code_frontend_str, 'css')
+        code_frontend_js = extract_code(code_frontend_str, 'javascript')
 
         # Documentation for backend
         conv7 = ConversationWrapper(self.frontend_doc, self.frontend_test)
         starter = f"""
         Write down questions you need to know in order to create an excellent and detailed documentation for the
-        database layer of the application. The project you are working on is about: {tasks['database']}.
-        """
+        frontend layer of the application. The project you are working on is about: {tasks['frontend']}.
+        """ #TODO: maybe provide actual project description instead of task
+
         documentation_frontend = conv7.start(starter)
-        self.orchestrator.inject_message(documentation_frontend, kind="human")
+        self.orchestrator.inject_message(documentation_frontend, kind="human") #QUESTION: what is this doing ??
--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ import json
 
 from typing import Dict
 from pathlib import Path
-
+import re
 
 def write_str_to_file(string: str, full_path: Path) -> str:
     with open(full_path, "w") as file:
@@ -20,3 +20,35 @@ def extract_json_from_str(text: str) -> Dict:
         return json.loads(extracted)
     else:
         raise ValueError("No valid JSON found")
+
+def extract_code(input_string, language):
+    """
+    Extracts a code section from the input string that starts with ```<language>
+    and ends with ```.
+    
+    :param input_string: The string to be parsed.
+    :param language: The language specifier (e.g., 'json', 'python').
+    :return: Extracted code section or an empty string if not found.
+    """
+    pattern = f"```{language}(.*?)```"
+    match = re.search(pattern, input_string, re.DOTALL)
+
+    if match:
+        return match.group(1).strip()
+    else:
+        return ""
+    
+def output_format(language: str, code_only: bool) -> str:
+    """
+    Generates a markdown code snippet with a specified programming language.
+
+    :param language: The language for the code snippet (e.g., 'python', 'javascript').
+    :param code_only: A boolean to force that only the code block is outputed.
+    :return: A string containing the formatted markdown code snippet.
+    """
+    instructions = f'The output should be a markdown code snippet, starting with "```{language}" and ending with "```".'
+    if code_only:
+        force_code = 'Only output this markdown code snippet. Do not output any additional comments.'
+        return instructions+' '+force_code
+
+    return instructions
