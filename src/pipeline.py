0e32528f8e3f95d1913e8332d73f513ec9ddc9c4
benediktpri <felix.2001@web.de>
2023-12-21 16:06:14 +0100

    parser v1

--- src/pipeline.py
+++ src/pipeline.py
@@ -26,6 +26,7 @@ class Pipeline:
                     name=agent["name"],
                     model=agent["model"],
                     temperature=agent["temperature"],
+                    parser=agent["parser"],
                     prompt=self.root / f"src/characters/{agent['varname']}.txt",
                 ),
             )
@@ -42,12 +43,15 @@ class Pipeline:
             prompt = f.read()
 
         tasks = self.orchestrator.answer(prompt)
-        tasks = extract_json_from_str(tasks)
+        # print("PLAIN TASKS: "+ tasks)
+        tasks = extract_json(tasks, [("database",str),("backend", str),("frontend",str)])
+        
 
         # Start database development
         conv2 = ConversationWrapper(self.database_dev, self.database_test)
         code_database_str = conv2.start("These are the requirements: "+tasks["database"]+' '+output_format('sql', True))  
-        code_database = extract_code(code_database_str, 'sql') # do something with the code
+        
+        # code_database = extract_code(code_database_str, 'sql') # do something with the code. code is now extraced in agents.py. however not complely plain there becaus langague is in string
 
         # Documentation for database
         conv3 = ConversationWrapper(self.database_doc, self.database_test)
@@ -56,42 +60,35 @@ class Pipeline:
         database layer of the application. The project you are working on is about: {tasks['database']}.
         """
         documentation_database = conv3.start(starter)
-        self.orchestrator.inject_message(documentation_database, kind="human") #QUESTION: what is this doing ??
+        self.orchestrator.inject_message(documentation_database, kind="human")
 
         # Start backend development
         conv4 = ConversationWrapper(self.backend_dev, self.backend_test)
         code_backend_str = conv4.start("These are the requirements: "+tasks["backend"]+' '+output_format('python', True))  
-        code_backend = extract_code(code_backend_str, 'python') # do something with the code
-
-        #TODO: what about the error logs? they still need to be added to conversation at some point. 
-        # In my opinon conversation starts too early. Backend Dev should first develop, then code is executed by us, then conversation with tester starts. 
-        # Opposed to initial design the tester now has two "roles": testing and answering to the doc. Maybe define seperate role prompts?
+        # code_backend = extract_code(code_backend_str, 'python') # do something with the code
 
         # Documentation for backend
         conv5 = ConversationWrapper(self.backend_doc, self.backend_test)
         starter = f"""
-        Write down questions you need to know in order to create an excellent and detailed documentation for the 
+        Beginn by aksing some questions you need to know in order to create an excellent and detailed documentation for the 
         backend layer of the application. The project you are working on is about: {tasks['backend']}.
-        """#TODO: maybe provide actual project description instead of task
+        """
         documentation_backend = conv5.start(starter)
-        self.orchestrator.inject_message(documentation_backend, kind="human") #QUESTION: what is this doing ??
-        #QUESTION: I am unsure when the code that is supposed to be documented enters this conversation? Is this missing?
-        # Or is the idea that the tester alone can look at the code from prev. conversation and answers are generated this way?
-        # Wouldn't we get much better answers if we just give the code to the doc aswell and do not do a conversation here?
-
+        self.orchestrator.inject_message(documentation_backend, kind="human") 
         # Start backend development
         conv6 = ConversationWrapper(self.frontend_dev, self.frontend_test)
         code_frontend_str = conv6.start("These are the requirements: "+tasks["database"]+output_format('html',True)+"Do this aswell for the 'css' and 'javascript' code.")  # do something with the code
-        code_frontend_html = extract_code(code_frontend_str, 'html')
-        code_frontend_css = extract_code(code_frontend_str, 'css')
-        code_frontend_js = extract_code(code_frontend_str, 'javascript')
+        # code_frontend_html = extract_code(code_frontend_str, 'html')
+        # code_frontend_css = extract_code(code_frontend_str, 'css')
+        # code_frontend_js = extract_code(code_frontend_str, 'javascript') 
+        # code extraction now in agents.py. however there code is written into one long string for conversation with comment about language
 
         # Documentation for backend
         conv7 = ConversationWrapper(self.frontend_doc, self.frontend_test)
         starter = f"""
-        Write down questions you need to know in order to create an excellent and detailed documentation for the
+        Beginn by aksing some questions you need to know in order to create an excellent and detailed documentation for the
         frontend layer of the application. The project you are working on is about: {tasks['frontend']}.
-        """ #TODO: maybe provide actual project description instead of task
+        """
 
         documentation_frontend = conv7.start(starter)
-        self.orchestrator.inject_message(documentation_frontend, kind="human") #QUESTION: what is this doing ??
+        self.orchestrator.inject_message(documentation_frontend, kind="human")
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,10 @@
 import json
 
-from typing import Dict
 from pathlib import Path
 import re
+
+from pydantic import BaseModel, ValidationError
+from typing import Any, Tuple, List, Dict
 
 def write_str_to_file(string: str, full_path: Path) -> str:
     with open(full_path, "w") as file:
@@ -21,6 +23,7 @@ def extract_json_from_str(text: str) -> Dict:
     else:
         raise ValueError("No valid JSON found")
 
+
 def extract_code(input_string, language):
     """
     Extracts a code section from the input string that starts with ```<language>
@@ -38,6 +41,7 @@ def extract_code(input_string, language):
     else:
         return ""
     
+
 def output_format(language: str, code_only: bool) -> str:
     """
     Generates a markdown code snippet with a specified programming language.
@@ -52,3 +56,72 @@ def output_format(language: str, code_only: bool) -> str:
         return instructions+' '+force_code
 
     return instructions
+
+
+def create_model(name: str, fields: List[Tuple[str, Any]]) -> type:
+    annotations: Dict[str, Any] = {}
+    defaults: Dict[str, Any] = {}
+    for field_name, field_type in fields:
+        annotations[field_name] = field_type
+        defaults[field_name] = ...
+    return type(name, (BaseModel,), {'__annotations__': annotations, '__default__': defaults})
+
+
+def extract_json(input_str: str, fields: List[Tuple[str, Any]]) -> dict:
+    # Create a dynamic model
+    DynamicModel = create_model('DynamicModel', fields)
+
+    # Extract JSON string from the input
+    json_str_match = re.search(r'```json\s*(.*?)```', input_str, re.DOTALL)
+    if not json_str_match:
+        raise ValueError("No JSON string found in the input")
+
+    json_str = json_str_match.group(1).strip()
+
+    # Parse and return the JSON data using the dynamic model
+    try:
+        parsed_data = DynamicModel.model_validate_json(json_str)
+        return parsed_data.model_dump()
+    except ValidationError as e:
+        raise ValueError(f"Invalid JSON data: {e}")
+
+
+def parse_response(response:str, pos: int, parser: dict):
+    if parser[str(pos)] == False:
+         return response
+    if parser["type"] == "code":
+        for language in parser["fields"]:
+            response = language+":\n"+extract_code(response, language)
+            # print("code parsed")
+    elif parser["type"] == "json":
+            response = extract_json(response, eval(parser["fields"]))
+            # print("JSON parsed")
+    else:
+         raise("Invalid type in parser. Use code or json")
+    return response # either the Code string or json
+
+
+# test_input ="""```json
+# {
+#         "accepted": true,
+#         "text": "The database system to be used is PostgreSQL. The database schema for storing the user's email and name will have a table named 'newsletter_signup' with columns 'id' (integer, primary key), 'email' (string), and 'name' (string). The 'email' field will have a unique constraint to ensure each email is unique. The 'name' field will have a length constraint of maximum 100 characters. There will be authentication and authorization mechanisms using API tokens for accessing the API endpoints. The expected HTTP request methods for the CRUD operations are POST for create, GET for read, PUT for update, and DELETE for delete. Input validation will be required to ensure that the email is in a valid format and that the name is not empty. Error handling will include returning appropriate HTTP status codes for different scenarios such as validation errors or database operation failures."
+# }
+# ```
+# """
+
+
+# test_fields=[("accepted", bool), ("text", str)]
+# test = extract_json(test_input, test_fields)
+# print(type(test))
+# print(test)
+# print("")
+# print(test["text"])
+
+
+# test_respone="This is a respone."
+# with open("src/setup/agents.json", "r") as file:
+#             config = json.load(file)
+# for agent in config:
+#     if agent.get("varname") == "database_test":
+#          parser = agent["parser"]
+# print(parse_response(test_input, 0, parser))
